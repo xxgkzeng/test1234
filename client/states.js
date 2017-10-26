2bd998b0175c33549038ae15009e7b10c632be76
Kent Zeng <Kent.Zeng@flex.com>
2017-10-25 23:54:58 -0700

    [Studio] Add screen "Login-Classic"

--- client/states.js
+++ client/states.js
@@ -33,5 +33,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.loginclassic', {
+        views: {
+            app: {
+                controller: 'app_loginclassic',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.loginclassic');
+                }
+            }
+        }
     });
 });
